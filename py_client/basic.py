dbc0c47d866e3f2fa7f6f52b53066325dd7cda7f
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2024-12-27 15:54:42 +0600

    Run Django Project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,12 +1,15 @@
 # Python API Client
 import requests
 
-endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/status/200/"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 get_response = requests.get(endpoint, json={"query":"Hello World"})    # HTTP Request
 
 print(get_response.text)  # print raw text response
-
-print(get_response.json())
 print(get_response.status_code)
+
+
+# print(get_response.json())
+# print(get_response.status_code)
