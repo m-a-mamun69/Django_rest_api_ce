7a3442b17d49b4c9ee648cf1be68ff9885d2cdcd
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2024-12-28 12:09:28 +0600

    Create First Api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,7 +3,7 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, json={"query":"Hello World"})    # HTTP Request
 
@@ -11,5 +11,5 @@ print(get_response.text)  # print raw text response
 print(get_response.status_code)
 
 
-# print(get_response.json())
+print(get_response.json()['message'])
 # print(get_response.status_code)
