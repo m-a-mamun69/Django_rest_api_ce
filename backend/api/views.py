488e66d032d90ca53eb84ea13a9b5bba3dfa5803
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2024-12-30 13:21:50 +0600

    Django Model instance as Api Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,22 +1,31 @@
 
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    # Request -> HttpRequest -> Django
-    # print(dir(request))
-    # request.body
-    body = request.body   # byte string JSON data
+
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
 
-    # return JsonResponse({"message": "Hi There, this is your Django Api Rasponce!!"})
+    # # return JsonResponse({"message": "Hi There, this is your Django Api Rasponce!!"})
+    #     # Request -> HttpRequest -> Django
+    # # print(dir(request))
+    # # request.body
+    # body = request.body   # byte string JSON data
+    # data = {}
+    # try:
+    #     data = json.loads(body)
+    # except:
+    #     pass
+    # print(data)
+    # data['params'] = dict(request.GET)
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
