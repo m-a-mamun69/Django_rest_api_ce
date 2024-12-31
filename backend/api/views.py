d726852ee4e5f242634e0ff81f130a4b61d97ede
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2024-12-31 12:04:53 +0600

    Django model instance to dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 
 import json
 from django.http import JsonResponse
+from django.forms.models import model_to_dict
 from products.models import Product
 
 # Create your views here.
@@ -9,10 +10,7 @@ def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id','title','price'])
     return JsonResponse(data)
 
     # # return JsonResponse({"message": "Hi There, this is your Django Api Rasponce!!"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -7,9 +7,9 @@ endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello World"})    # HTTP Request
 
-print(get_response.text)  # print raw text response
-print(get_response.status_code)
+# print(get_response.text)  # print raw text response
+# print(get_response.status_code)
 
-
+print(get_response.json())
 # print(get_response.json()['message'])
 # print(get_response.status_code)
