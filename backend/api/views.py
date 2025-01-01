b7b30b7d6605e97a0452f5dec1ee528b74a665af
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2025-01-01 19:16:58 +0600

    Insert Data Django rest Framework Views

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,17 +7,22 @@ from products.models import Product
 from products.serializers import ProductSerializer
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=['id','title','price','sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
+    # instance = Product.objects.all().order_by("?").first()
+    # data = {}
+    # if instance:
+    #     # data = model_to_dict(instance, fields=['id','title','price','sale_price'])
+    #     data = ProductSerializer(instance).data
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        # data = serializer.data
+        return Response(serializer.data)
+    return Response({"Invalid":"not good data"}, status=400)
 
     # # return JsonResponse({"message": "Hi There, this is your Django Api Rasponce!!"})
     #     # Request -> HttpRequest -> Django
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"query":"Hello World"})    # HTTP Request
+get_response = requests.post(endpoint, json={"title":"Hello World", "content":"This is Content"})    # HTTP Request
 
 # print(get_response.text)  # print raw text response
 # print(get_response.status_code)
