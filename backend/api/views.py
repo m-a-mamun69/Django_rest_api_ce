7d5dc29ed5d0abcf864ca695bb1ba7f73b2b0e83
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2025-01-01 11:14:45 +0600

    Django Rest Framework Model Serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,16 +4,19 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.models import Product
 
+from products.serializers import ProductSerializer
+
 # Create your views here.
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=['id','title','price'])
+    if instance:
+        # data = model_to_dict(instance, fields=['id','title','price','sale_price'])
+        data = ProductSerializer(instance).data
     return Response(data)
 
     # # return JsonResponse({"message": "Hi There, this is your Django Api Rasponce!!"})
